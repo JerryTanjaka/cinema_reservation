a6aa64a3553ee08967058fb85df50ea135da96af
unknown <tanjakajerryjacques@gmail.com>
2026-08-03 09:35:34 +0300

    feat(model) : add Reservation model with one joinTable refer to seats

--- src/main/java/com/example/demo/model/Reservation.java
+++ src/main/java/com/example/demo/model/Reservation.java
@@ -1,4 +1,42 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "reservations")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private Instant createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "projection_id", nullable = false)
+    private Projection projection;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToMany
+    @JoinTable(
+            name = "reservation_seat",
+            joinColumns = @JoinColumn(name = "reservation_id"),
+            inverseJoinColumns = @JoinColumn(name = "seat_id")
+    )
+    private List<Seat> seats = new ArrayList<>();
 }
