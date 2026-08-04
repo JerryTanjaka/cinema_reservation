292b953d1c29c910bd42f3619af8393fbbd81f30
Manda Tiavina <solontianamandanirina@gmail.com>
2026-08-04 21:13:29 +0300

    refactor : format code with google-java-format

--- src/main/java/com/example/demo/repository/SeatRepository.java
+++ src/main/java/com/example/demo/repository/SeatRepository.java
@@ -1,10 +1,7 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Seat;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface SeatRepository extends JpaRepository<Seat, UUID> {
-
-}
+public interface SeatRepository extends JpaRepository<Seat, UUID> {}
